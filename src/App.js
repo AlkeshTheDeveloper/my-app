9e4523356940f3ce7583537e137f4f3aa16c5b20
AlkeshTheDeveloper <alkeshsakre110791@gmail.com>
2021-11-09 09:41:45 +0530

    newchanges

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import { useState } from 'react';
-import './App.css';
-import Navigation from './components/Navigation';
+import React from 'react'
 
 function App() {
-  let [count,setCount]=useState(0)
-
- 
   return (
     <div>
-      <Navigation />
-
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +</button>
-        <span>
-          
-          {count}
-        </span>
-      <button  onClick={()=>{setCount(count-1)}} disabled={count===0?true:false}>-</button>
-     
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
